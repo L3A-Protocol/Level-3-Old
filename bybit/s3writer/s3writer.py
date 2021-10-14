79a161fea0bbcb8bd88f87f3927877b72fa4672c
Harut Grigoryan <harut.grigoryan@4r-soft.com>
2021-10-14 22:26:01 +0400

    Use a thread to flush data to S3

--- bybit/s3writer/s3writer.py
+++ bybit/s3writer/s3writer.py
@@ -11,6 +11,101 @@ import math
 import boto3
 from botocore.client import Config
 
+from threading import Timer, Thread, Lock
+from time import time
+
+# Variables
+
+load_dotenv(override=True)
+c_bin_path  = os.getenv("C_BINARY_PATH", None)
+topic       = os.getenv("TOPIC", None)
+bucket_name = os.getenv("BUCKET_NAME", None)
+
+access_key_id       = os.getenv("AWS_ACCESS_KEY_ID", None)
+access_secret_key   = os.getenv("AWS_SECRET_ACCESS_KEY", None)
+feed_interval       = int(os.getenv("FEED_INTERVAL", 100))
+
+s3 = boto3.resource(
+    's3',
+    aws_access_key_id=access_key_id,
+    aws_secret_access_key=access_secret_key,
+    config=Config(signature_version='s3v4')
+)
+
+def get_current_timestamp():
+    dt = datetime.now(timezone.utc)
+    utc_time = dt.replace(tzinfo=timezone.utc)
+    return utc_time.timestamp()
+
+old_flush_timestamp = get_current_timestamp()
+FIFO = f'/tmp/{topic}'
+raw_lines = ''
+number_of_lines = 0
+
+mutex = Lock()
+
+# Functions
+
+def has_minute_increased(old_ts, new_ts):
+
+    old_min = datetime.utcfromtimestamp(old_ts).strftime('%M')
+    new_min = datetime.utcfromtimestamp(new_ts).strftime('%M')
+
+    if old_min != new_min:
+        return "Flush"
+    else:
+        return "Go"
+
+def s3_bucket_folders(data, _symbol, year, month, day):
+    return r'true-alpha/exchange=ByBit/'+data+r'/symbol='+_symbol+'/year='+str(year)+'/month=' + str(month) + '/day=' + str(day) + '/'
+
+def s3_bucket_raw_data_folders(topic, year, month, day):
+    return r'raw-data/exchange=ByBit/'+topic+r'/year='+str(year)+'/month=' + str(month) + '/day=' + str(day) + '/'
+
+def verify_feed_frequency (number_of_lines, period):
+    global feed_interval
+    expected_feed = math.floor(period / feed_interval)
+
+    log_line = {"period":period,"lines_read":number_of_lines,"expected":expected_feed}
+    print(log_line)
+
+    if expected_feed > number_of_lines:
+        # Allert low feed frequency
+        print(f'ERROR: low feed frequency: {number_of_lines}. Expected {expected_feed} for the period of {period} milliseconds')
+    else:
+        print(f'Expected feed frequency: {number_of_lines}. Expected {expected_feed} for the period of {period} milliseconds')
+
+def process_raw_line(line):
+    global raw_lines
+    global number_of_lines
+    # global old_flush_timestamp
+
+    mutex.acquire()
+    try:
+        raw_lines = raw_lines + line
+        number_of_lines += 1
+    finally:
+        mutex.release()
+
+    # utc_timestamp = get_current_timestamp()
+
+    # year = datetime.utcfromtimestamp(utc_timestamp).strftime('%Y')
+    # month = datetime.utcfromtimestamp(utc_timestamp).strftime('%m')
+    # day = datetime.utcfromtimestamp(utc_timestamp).strftime('%d')
+
+    # seq = (int)(utc_timestamp * 1000000)
+
+    # if has_minute_increased(old_flush_timestamp, utc_timestamp) == "Flush":
+    #     folders = s3_bucket_raw_data_folders(topic, year, month, day)
+    #     s3.Bucket(bucket_name).put_object(Key=f'{folders}{seq}', Body=raw_lines)
+
+    #     period = math.floor((utc_timestamp - old_flush_timestamp) * 1000)
+    #     verify_feed_frequency(number_of_lines, period)
+    #     old_flush_timestamp = utc_timestamp
+
+    #     raw_lines = ''
+    #     number_of_lines = 0
+
 def readline(fifo):
     line = ''
     try:
@@ -22,14 +117,43 @@ def readline(fifo):
         pass
     return line
 
-load_dotenv(override=True)
-c_bin_path  = os.getenv("C_BINARY_PATH", None)
-topic       = os.getenv("TOPIC", None)
-bucket_name = os.getenv("BUCKET_NAME", None)
+def flush_thread_function():
+    global raw_lines
+    global number_of_lines
+    global old_flush_timestamp
 
-access_key_id       = os.getenv("AWS_ACCESS_KEY_ID", None)
-access_secret_key   = os.getenv("AWS_SECRET_ACCESS_KEY", None)
-feed_interval       = int(os.getenv("FEED_INTERVAL", 100))
+    mutex.acquire()
+    try:
+        Timer(int(time()/60)*60+60 - time(), flush_thread_function).start ()
+
+        utc_timestamp = get_current_timestamp()
+        print(utc_timestamp)
+
+        # if 0 == old_flush_timestamp:
+        #     old_flush_timestamp = utc_timestamp
+
+        year = datetime.utcfromtimestamp(utc_timestamp).strftime('%Y')
+        month = datetime.utcfromtimestamp(utc_timestamp).strftime('%m')
+        day = datetime.utcfromtimestamp(utc_timestamp).strftime('%d')
+
+        seq = (int)(utc_timestamp * 1000000)
+
+        folders = s3_bucket_raw_data_folders(topic, year, month, day)
+        s3.Bucket(bucket_name).put_object(Key=f'{folders}{seq}', Body=raw_lines)
+
+        period = math.floor((utc_timestamp - old_flush_timestamp) * 1000)
+        verify_feed_frequency(number_of_lines, period)
+        old_flush_timestamp = utc_timestamp
+
+        raw_lines = ''
+        number_of_lines = 0
+    finally:
+        mutex.release()
+
+# Functional code
+
+x = Thread(target=flush_thread_function, args=())
+x.start()
 
 if not c_bin_path:
     print ("The binary path is not specified")
@@ -54,83 +178,6 @@ if not access_key_id:
 if not access_secret_key:
     print (f"AWS secret key is not specified")
     sys.exit()
-
-s3 = boto3.resource(
-    's3',
-    aws_access_key_id=access_key_id,
-    aws_secret_access_key=access_secret_key,
-    config=Config(signature_version='s3v4')
-)
-
-def get_current_timestamp():
-    dt = datetime.now(timezone.utc)
-    utc_time = dt.replace(tzinfo=timezone.utc)
-    return utc_time.timestamp()
-
-old_flush_timestamp = 0
-
-FIFO = f'/tmp/{topic}'
-
-raw_lines = ''
-number_of_lines = 0
-
-def has_minute_increased(old_ts, new_ts):
-
-    old_min = datetime.utcfromtimestamp(old_ts).strftime('%M')
-    new_min = datetime.utcfromtimestamp(new_ts).strftime('%M')
-
-    if old_min != new_min:
-        return "Flush"
-    else:
-        return "Go"
-
-def s3_bucket_folders(data, _symbol, year, month, day):
-    return r'true-alpha/exchange=ByBit/'+data+r'/symbol='+_symbol+'/year='+str(year)+'/month=' + str(month) + '/day=' + str(day) + '/'
-
-def s3_bucket_raw_data_folders(topic, year, month, day):
-    return r'raw-data/exchange=ByBit/'+topic+r'/year='+str(year)+'/month=' + str(month) + '/day=' + str(day) + '/'
-
-symbol_ws = topic
-
-def verify_feed_frequency (number_of_lines, period):
-    global feed_interval
-    expected_feed = math.floor(period / feed_interval)
-    if expected_feed > number_of_lines:
-        # Allert low feed frequency
-        print(f'ERROR: low feed frequency: {number_of_lines}. Expected {expected_feed} for the period of {period} milliseconds')
-    else:
-        print(f'Expected feed frequency: {number_of_lines}. Expected {expected_feed} for the period of {period} milliseconds')
-
-def process_raw_line(line):
-    global raw_lines
-    global number_of_lines
-    global old_flush_timestamp
-
-    raw_lines = raw_lines + line
-    number_of_lines += 1
-
-    utc_timestamp = get_current_timestamp()
-    if 0 == old_flush_timestamp:
-        old_flush_timestamp = utc_timestamp
-
-    year = datetime.utcfromtimestamp(utc_timestamp).strftime('%Y')
-    month = datetime.utcfromtimestamp(utc_timestamp).strftime('%m')
-    day = datetime.utcfromtimestamp(utc_timestamp).strftime('%d')
-
-    seq = (int)(utc_timestamp * 1000000)
-
-    if has_minute_increased(old_flush_timestamp, utc_timestamp) == "Flush":
-        folders = s3_bucket_raw_data_folders(topic, year, month, day)
-        s3.Bucket(bucket_name).put_object(Key=f'{folders}{seq}', Body=raw_lines)
-        # number_of_lines_log = {"number_of_lines":number_of_lines}
-        # print(number_of_lines_log)
-
-        period = math.floor((utc_timestamp - old_flush_timestamp) * 1000)
-        verify_feed_frequency(number_of_lines, period)
-        old_flush_timestamp = utc_timestamp
-
-        raw_lines = ''
-        number_of_lines = 0
 
 try:
     os.system(f'{c_bin_path} --topic {topic} &')
