362251981170cf08d5e9e1325a45273b4766c041
Harut Grigoryan <harut.grigoryan@4r-soft.com>
2021-10-14 21:01:25 +0400

    Leave raw feed only. Error on low feed frequency.

--- bybit/s3writer/s3writer.py
+++ bybit/s3writer/s3writer.py
@@ -9,8 +9,6 @@ from datetime import timezone
 
 import math
 import boto3
-import numpy as np
-import pandas
 from botocore.client import Config
 
 def readline(fifo):
@@ -31,6 +29,7 @@ bucket_name = os.getenv("BUCKET_NAME", None)
 
 access_key_id       = os.getenv("AWS_ACCESS_KEY_ID", None)
 access_secret_key   = os.getenv("AWS_SECRET_ACCESS_KEY", None)
+feed_interval       = int(os.getenv("FEED_INTERVAL", 100))
 
 if not c_bin_path:
     print ("The binary path is not specified")
@@ -63,68 +62,19 @@ s3 = boto3.resource(
     config=Config(signature_version='s3v4')
 )
 
-book = pandas.DataFrame
-snap_shot = pandas.DataFrame
-old_timestamp = 0
-old_raw_data_timestamp = 0
+def get_current_timestamp():
+    dt = datetime.now(timezone.utc)
+    utc_time = dt.replace(tzinfo=timezone.utc)
+    return utc_time.timestamp()
+
+old_flush_timestamp = 0
 
 FIFO = f'/tmp/{topic}'
 
-"""
-The goal here is to capture all LOB events accurately for all pairs.
-Microservice for ByBit, Web Socket limit order book snapshot then events.
-All messages a grouped together in minute buckets, using their timestamps
-this is so the files are exactly replicated, and recombined later. This  is for
-sake of data accuracy.
-We are buffering with pandas in case we need stats in the future, and for data checks.
-This article inspired this approach.
-https://medium.com/floating-point-group/collecting-and-distributing-high-resolution-crypto-market-data-with-ecs-s3-athena-lambda-and-9f0bd5ab3452
-:return:
-"""
-
-dtypes = np.dtype([
-    ('ws_seq_id', np.uint64),
-    ('seq_id', np.uint64),
-    ('action_id', np.uint8),
-    ('price', np.float32),
-    ('size', np.float32),
-    ('symbol', np.str),
-    ('side', np.uint8),
-    ('year', np.uint16),
-    ('month', np.uint8),
-    ('day', np.uint8),
-    ('exchange_unix_time', np.uint64),
-    ('our_unix_time', np.uint64)
-])
-data = np.zeros(0, dtype=dtypes)
-book = pandas.DataFrame(data)
-dtypes = np.dtype([
-    ('ws_seq_id', np.uint64),
-    ('seq_id', np.uint64),
-    ('price', np.float32),
-    ('size', np.float32),
-    ('symbol', np.str),
-    ('side', np.uint8),
-    ('year', np.uint16),
-    ('month', np.uint8),
-    ('day', np.uint8),
-    ('exchange_unix_time', np.uint64),
-    ('our_unix_time', np.uint64)
-])
-data = np.zeros(0, dtype=dtypes)
-snap_shot = pandas.DataFrame(data)
-
 raw_lines = ''
+number_of_lines = 0
 
 def has_minute_increased(old_ts, new_ts):
-    """
-    compares the minute from the unix time stamp, if it increments returns true
-    :param old_ts:
-    :param new_ts:
-    :return:
-    """
-    if old_ts == 0:
-        return "Go"
 
     old_min = datetime.utcfromtimestamp(old_ts).strftime('%M')
     new_min = datetime.utcfromtimestamp(new_ts).strftime('%M')
@@ -142,113 +92,26 @@ def s3_bucket_raw_data_folders(topic, year, month, day):
 
 symbol_ws = topic
 
-def process_json_data(json_string):
-    global book
-    global snap_shot
-    global old_timestamp
-    global s3
-
-    now = datetime.utcnow()
-    data = str_to_json(json_string)
-
-    if not 'type' in data:
-        # print("type not found")
-        return 1
-
-    seq = data['cross_seq']
-    timestamp_short = math.trunc(int(data['timestamp_e6'])/1000000)
-    timestamp = data['timestamp_e6']
-    year = datetime.utcfromtimestamp(timestamp_short).strftime('%Y')
-    month = datetime.utcfromtimestamp(timestamp_short).strftime('%m')
-    day = datetime.utcfromtimestamp(timestamp_short).strftime('%d')
-    if data['type'] == 'delta':
-        data = data['data']
-        deletes = data['delete']
-        updates = data['update']
-        inserts = data['insert']
-        if deletes:
-            for delete in deletes:
-                book = book.append({'ws_seq_id': seq,
-                                    'seq_id': delete['id'],
-                                    'action_id': 1,  # Delete
-                                    'price': delete['price'],
-                                    'size': 0,
-                                    'symbol': symbol_ws,
-                                    'side': 1 if delete['side'] == 'Buy' else 2,
-                                    'year': int(year),
-                                    'month': int(month),
-                                    'day': int(day),
-                                    'exchange_unix_time': timestamp,
-                                     'our_unix_time': now}, ignore_index=True)
-        if updates:
-            for update in updates:
-                book = book.append({'ws_seq_id': seq,
-                                    'seq_id': update['id'],
-                                    'action_id': 2,  # Update
-                                    'price': update['price'],
-                                    'size': update['size'],
-                                    'symbol': symbol_ws,
-                                    'side': 1 if update['side'] == 'Buy' else 2,
-                                    'year': int(year),
-                                    'month': int(month),
-                                    'day': int(day),
-                                    'exchange_unix_time': timestamp,
-                                    'our_unix_time': now}, ignore_index=True)
-        if inserts:
-            for insert in inserts:
-                book = book.append({'ws_seq_id': seq,
-                                    'seq_id': insert['id'],
-                                    'action_id': 3,  # Insert
-                                    'price': insert['price'],
-                                    'size': insert['size'],
-                                    'symbol': symbol_ws,
-                                    'side': 1 if insert['side'] == 'Buy' else 2,
-                                    'year': int(year),
-                                    'month': int(month),
-                                    'day': int(day),
-                                    'exchange_unix_time': timestamp,
-                                    'our_unix_time': now}, ignore_index=True)
-        # TODO: Finish s3 flush code for LOB
-        if has_minute_increased(old_timestamp, timestamp) == "Flush":
-            # print(str(old_timestamp) + ' flush ' + str(timestamp))
-            book_json = book.to_json(orient='records', lines=True)
-            folders = s3_bucket_folders('lob_events', symbol_ws, year, month, day)
-            s3.Bucket(bucket_name).put_object(Key=f'{folders}{seq}.json', Body=str(book_json))
-            book.drop(book.index, inplace=True)
-        old_timestamp = timestamp
-    elif data['type'] == 'snapshot':
-        data = data['data']
-        limit_order_book = data['order_book']
-        for price_level in limit_order_book:
-            snap_shot = snap_shot.append({
-                'ws_seq_id': seq,
-                'seq_id': price_level['id'],
-                'price': price_level['price'],
-                'size': price_level['size'],
-                'symbol': symbol_ws,
-                'side': 1 if price_level['side'] == 'Buy' else 2,
-                'year': int(year),
-                'month': int(month),
-                'day': int(day),
-                'exchange_unix_time': timestamp,
-                'our_unix_time': now}, ignore_index=True)
-        snap_shot_json = snap_shot.to_json(orient='records', lines=True)
-        folders = s3_bucket_folders('lob-snapshot', symbol_ws, year, month, day)
-        s3.Bucket(bucket_name).put_object(Key=f'{folders}{seq}.json', Body=str(snap_shot_json))
-        snap_shot.drop(snap_shot.index, inplace=True)  # Clear Snap Shot Buffer
+def verify_feed_frequency (number_of_lines, period):
+    global feed_interval
+    expected_feed = math.floor(period / feed_interval)
+    if expected_feed > number_of_lines:
+        # Allert low feed frequency
+        print(f'ERROR: low feed frequency: {number_of_lines}. Expected {expected_feed} for the period of {period} milliseconds')
     else:
-        print("unknown - type!!!")
-        # TODO: Need to close down Micro Service Task
+        print(f'Expected feed frequency: {number_of_lines}. Expected {expected_feed} for the period of {period} milliseconds')
 
 def process_raw_line(line):
     global raw_lines
-    global old_raw_data_timestamp
+    global number_of_lines
+    global old_flush_timestamp
 
     raw_lines = raw_lines + line
+    number_of_lines += 1
 
-    dt = datetime.now(timezone.utc)
-    utc_time = dt.replace(tzinfo=timezone.utc)
-    utc_timestamp = utc_time.timestamp()
+    utc_timestamp = get_current_timestamp()
+    if 0 == old_flush_timestamp:
+        old_flush_timestamp = utc_timestamp
 
     year = datetime.utcfromtimestamp(utc_timestamp).strftime('%Y')
     month = datetime.utcfromtimestamp(utc_timestamp).strftime('%m')
@@ -256,13 +119,18 @@ def process_raw_line(line):
 
     seq = (int)(utc_timestamp * 1000000)
 
-    if has_minute_increased(old_raw_data_timestamp, utc_timestamp) == "Flush":
-        # print(str(old_raw_data_timestamp) + ' flush ' + str(utc_timestamp))
+    if has_minute_increased(old_flush_timestamp, utc_timestamp) == "Flush":
         folders = s3_bucket_raw_data_folders(topic, year, month, day)
         s3.Bucket(bucket_name).put_object(Key=f'{folders}{seq}', Body=raw_lines)
-        raw_lines = ''
+        # number_of_lines_log = {"number_of_lines":number_of_lines}
+        # print(number_of_lines_log)
 
-    old_raw_data_timestamp = utc_timestamp
+        period = math.floor((utc_timestamp - old_flush_timestamp) * 1000)
+        verify_feed_frequency(number_of_lines, period)
+        old_flush_timestamp = utc_timestamp
+
+        raw_lines = ''
+        number_of_lines = 0
 
 try:
     os.system(f'{c_bin_path} --topic {topic} &')
@@ -293,12 +161,3 @@ with open(FIFO) as fifo:
             print (f'ERROR in process_raw_line: {ex}')
             continue
 
-        if not line.endswith('}\n'):
-            # not a valid JSON
-            continue
-
-        try:
-            process_json_data(line)
-        except Exception as ex:
-            print (f'ERROR in process_json_data: {ex}')
-            continue
