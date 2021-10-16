6c1e8a9268801190c0e6589f13e555ebd08289d5
Harut Grigoryan <harut.grigoryan@4r-soft.com>
2021-10-16 14:38:48 +0400

    Bybit task definitions

--- docker-login.sh
+++ docker-login.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo aws ecr get-login-password --region eu-west-1 | sudo docker login --username AWS --password-stdin 381452754685.dkr.ecr.eu-west-1.amazonaws.com
+sudo aws ecr get-login-password --region us-west-1 | sudo docker login --username AWS --password-stdin 381452754685.dkr.ecr.us-west-1.amazonaws.com
