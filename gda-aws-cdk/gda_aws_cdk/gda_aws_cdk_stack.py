0c1ddc776163a91b3eeec454a18cb1daebe2f16e
Harut Grigoryan <harut.grigoryan@4r-soft.com>
2021-10-29 22:11:51 +0400

    Create VPC and ECS Cluster

--- gda-aws-cdk/gda_aws_cdk/gda_aws_cdk_stack.py
+++ gda-aws-cdk/gda_aws_cdk/gda_aws_cdk_stack.py
@@ -1,20 +1,24 @@
-from aws_cdk import core as cdk
-
 # For consistency with other languages, `cdk` is the preferred import name for
 # the CDK's core module.  The following line also imports it as `core` for use
 # with examples from the CDK Developer's Guide, which are in the process of
 # being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
 
-from aws_cdk import aws_s3 as s3
+from aws_cdk import (core as cdk,
+                     aws_s3 as s3,
+                     aws_ec2 as ec2,
+                     aws_ecs as ecs,
+                     aws_ecs_patterns as ecs_patterns)
 
 class GdaAwsCdkStack(cdk.Stack):
 
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        bucket = s3.Bucket(self, "GDADataLake",
+            versioned=True,
+            removal_policy=cdk.RemovalPolicy.DESTROY,
+            auto_delete_objects=True)
 
-        bucket = s3.Bucket(self,
-            "gda-data-lake",
-            versioned=True,)
+        vpc = ec2.Vpc(self, "GDADataLakeVpc", max_azs=3)     # default is all AZs in region
+
+        cluster = ecs.Cluster(self, "GDADataLakeCluster", vpc=vpc)
