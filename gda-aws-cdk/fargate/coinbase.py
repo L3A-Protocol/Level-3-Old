c7dbd33fe24b0a8586f0f69021c00d6f706c14b1
Harut Grigoryan <harut.grigoryan@4r-soft.com>
2021-10-31 12:31:15 +0400

    Coinbase Construct

---
+++ gda-aws-cdk/fargate/coinbase.py
@@ -0,0 +1,16 @@
+import os
+
+from aws_cdk import (core as cdk,
+                     aws_s3 as s3,
+                     aws_ecs as ecs)
+
+from fargate.exchangebase import ExchangeBase
+
+EXCHNAGE_NAME       = 'Coinbase'
+BIN_PATH            = "/app/lws-coinbase"
+REPO_ARN            = "arn:aws:ecr:us-west-1:381452754685:repository/coinbase03"
+
+class CoinbaseConstruct(ExchangeBase):
+
+    def __init__(self, scope: cdk.Construct, id: str, bucket: s3.Bucket, cluster: ecs.Cluster, topic: str, **kwargs):
+        super().__init__(scope, id, EXCHNAGE_NAME, BIN_PATH, REPO_ARN, bucket, cluster, topic, **kwargs)
