1363bb2020415035c7e19a1f576d638fc2e214ea
Harut Grigoryan <harut.grigoryan@4r-soft.com>
2021-11-06 18:20:08 +0400

    Price Binance module

--- s3writer/priceinfo.py
+++ s3writer/priceinfo.py
@@ -1,6 +1,7 @@
 import datetime
 from log_json import log_json
 from osbot_utils.utils.Json import str_to_json
+from pricebinance import PriceBinance, TOPIC_BINANCE_BINANCE
 
 EX_BYBIT        = "ByBit"
 EX_BYBIT_USDT   = "ByBit-USDT"
@@ -19,8 +20,6 @@ TOPIC_BYBIT_USDT_TRADE  = "trade.BTCUSDT"
 TOPIC_COINBASE_BTCUSD   = "BTC-USD"
 TOPIC_COINBASE_ETHUSD   = "ETH-USD"
 
-TOPIC_BINANCE_BINANCE   = "binance"
-
 class PriceInfo(object):
     def __init__(self):
         self.log = log_json()
@@ -33,58 +32,6 @@ class PriceInfo(object):
             "timestamp" : timestamp       
         }
 
-    def process_bybit(self, topic:str, json_data):
-        symbol = "none"
-        price  = 0
-        timestamp = 0
-
-        if TOPIC_BYBIT_INSURANCE    == topic:
-            pass
-        elif TOPIC_BYBIT_KLINE      == topic:
-            pass
-        elif TOPIC_BYBIT_OB200      == topic:
-            pass
-        elif TOPIC_BYBIT_TRADE      == topic:
-            pass
-
-        return self.getJson(symbol=symbol, price=price, timestamp=timestamp)
-
-    def process_bybit_usdt(self, topic:str, json_data):
-        symbol = "none"
-        price  = 0
-        timestamp = 0
-
-        if TOPIC_BYBIT_USDT_CANDLE    == topic:
-            pass
-        elif TOPIC_BYBIT_USDT_OB200      == topic:
-            pass
-        elif TOPIC_BYBIT_USDT_TRADE      == topic:
-            pass
-
-        return self.getJson(symbol=symbol, price=price, timestamp=timestamp)
-
-    def process_coinbase(self, topic:str, json_data):
-        symbol = "none"
-        price  = 0
-        timestamp = 0
-
-        if TOPIC_COINBASE_BTCUSD    == topic:
-            pass
-        elif TOPIC_COINBASE_ETHUSD  == topic:
-            pass
-
-        return self.getJson(symbol=symbol, price=price, timestamp=timestamp)
-
-    def process_binance(self, topic:str, json_data):
-        symbol = "none"
-        price  = 0
-        timestamp = 0
-
-        if TOPIC_BINANCE_BINANCE    == topic:
-            pass
-
-        return self.getJson(symbol=symbol, price=price, timestamp=timestamp)
-
     def process_raw_data(self, exchange:str, topic:str, data):
         json_data = {}
         try:
@@ -93,10 +40,10 @@ class PriceInfo(object):
             self.log.create("ERROR", str(ex))
             return(None)
 
-        if EX_BYBIT         == exchange:        return self.process_bybit       (topic=topic, json_data=json_data)
-        if EX_BYBIT_USDT    == exchange:        return self.process_bybit_usdt  (topic=topic, json_data=json_data)
-        if EX_BINANCE       == exchange:        return self.process_binance     (topic=topic, json_data=json_data)
-        if EX_COINBASE      == exchange:        return self.process_coinbase    (topic=topic, json_data=json_data)
+        # if EX_BYBIT         == exchange:        return self.process_bybit       (topic=topic, json_data=json_data)
+        # if EX_BYBIT_USDT    == exchange:        return self.process_bybit_usdt  (topic=topic, json_data=json_data)
+        # if EX_COINBASE      == exchange:        return self.process_coinbase    (topic=topic, json_data=json_data)
+        if EX_BINANCE       == exchange:        return PriceBinance().process_json_data(topic=topic, json_data=json_data)
 
         self.log.create("ERROR", f'{exchange} EXCHANGE NOT SUPPOTED')
         return None
@@ -109,16 +56,17 @@ if __name__ == '__main__':
 
     raw_data = "\{\}"
 
-    print(info.process_raw_data(EX_BYBIT,TOPIC_BYBIT_INSURANCE,raw_data))
-    print(info.process_raw_data(EX_BYBIT,TOPIC_BYBIT_KLINE,raw_data))
-    print(info.process_raw_data(EX_BYBIT,TOPIC_BYBIT_OB200,raw_data))
-    print(info.process_raw_data(EX_BYBIT,TOPIC_BYBIT_TRADE,raw_data))
+    # print(info.process_raw_data(EX_BYBIT,TOPIC_BYBIT_INSURANCE,raw_data))
+    # print(info.process_raw_data(EX_BYBIT,TOPIC_BYBIT_KLINE,raw_data))
+    # print(info.process_raw_data(EX_BYBIT,TOPIC_BYBIT_OB200,raw_data))
+    # print(info.process_raw_data(EX_BYBIT,TOPIC_BYBIT_TRADE,raw_data))
 
-    print(info.process_raw_data(EX_BYBIT_USDT,TOPIC_BYBIT_USDT_CANDLE,raw_data))
-    print(info.process_raw_data(EX_BYBIT_USDT,TOPIC_BYBIT_USDT_OB200,raw_data))
-    print(info.process_raw_data(EX_BYBIT_USDT,TOPIC_BYBIT_USDT_TRADE,raw_data))
+    # print(info.process_raw_data(EX_BYBIT_USDT,TOPIC_BYBIT_USDT_CANDLE,raw_data))
+    # print(info.process_raw_data(EX_BYBIT_USDT,TOPIC_BYBIT_USDT_OB200,raw_data))
+    # print(info.process_raw_data(EX_BYBIT_USDT,TOPIC_BYBIT_USDT_TRADE,raw_data))
 
-    print(info.process_raw_data(EX_COINBASE,TOPIC_COINBASE_BTCUSD,raw_data))
-    print(info.process_raw_data(EX_COINBASE,TOPIC_COINBASE_ETHUSD,raw_data))
+    # print(info.process_raw_data(EX_COINBASE,TOPIC_COINBASE_BTCUSD,raw_data))
+    # print(info.process_raw_data(EX_COINBASE,TOPIC_COINBASE_ETHUSD,raw_data))
 
+    raw_data = "{\"stream\":\"btcusdt@aggTrade\",\"data\":{\"e\":\"aggTrade\",\"E\":1634390640539,\"a\":874355956,\"s\":\"BTCUSDT\",\"p\":\"60602.22\",\"q\":\"0.002\",\"f\":1546375311,\"l\":1546375311,\"T\":1634390640533,\"m\":false}}"
     print(info.process_raw_data(EX_BINANCE,TOPIC_BINANCE_BINANCE,raw_data))
